409f264f447ed368c675455175f620a316afc67b
flowsar <brahim.sar151@gmail.com>
2024-10-19 21:41:50 +0100

    create table users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,1 +1,4 @@
- 
+
+-- create users table with enum colum
+
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US' );
