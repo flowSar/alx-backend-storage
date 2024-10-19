ddc37bb17bd5453a6ded7a714a338e0c3fe0eb0f
flowsar <brahim.sar151@gmail.com>
2024-10-19 22:34:09 +0100

    List all bands with Glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, CASE 
-    WHEN split IS NOT NULL THEN (split - formed)
-    ELSE (2022 - formed)
-    END AS lifespan
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC; 
+GROUP BY origin
+ORDER BY nb_fans DESC;
