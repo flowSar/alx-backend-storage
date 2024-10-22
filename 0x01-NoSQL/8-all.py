2a66a71d9ca65cf5b9e5f324deead862a6acf085
flowsar <brahim.sar151@gmail.com>
2024-10-22 23:36:56 +0100

    style

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """MongoDB"""
-
 from pymongo import MongoClient
+
+
 def list_all(mongo_collection):
     """List all documents in a MongoDB collection."""
     documents = mongo_collection.find()
