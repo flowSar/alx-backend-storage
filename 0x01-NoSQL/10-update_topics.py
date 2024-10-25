48e1184ab5307301b632b54730762d251ca2a3a6
flowsar <brahim.sar151@gmail.com>
2024-10-25 23:37:56 +0100

    update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,4 +5,4 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """Updates the topics of a school document based on the name."""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
