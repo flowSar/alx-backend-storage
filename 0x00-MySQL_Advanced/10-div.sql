365ffa1e84f21f672d8459856dbe1fa3ea6b0abd
flowsar <brahim.sar151@gmail.com>
2025-01-27 01:12:33 +0100

    Create the SafeDiv function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,1 +1,18 @@
- 
+DELIMITER $$
+
+-- Create the SafeDiv function
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    -- Check if the second number (b) is 0
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        -- Return the result of a divided by b
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
+
