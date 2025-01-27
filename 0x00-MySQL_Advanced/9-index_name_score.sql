dc42f8d3af273b082622eb2d8c9ffcb434c30c36
flowsar <brahim.sar151@gmail.com>
2025-01-27 09:30:13 +0100

    Optimize search and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,6 @@
--- Create the index on the first letter of the name and the score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+--  Add a generated column for the first letter of the 'name'
+ALTER TABLE names 
+ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
 
+-- Create the composite index on 'first_letter' and 'score'
+CREATE INDEX idx_name_first_score ON names (first_letter, score);
